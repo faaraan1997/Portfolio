3a6b7e658bfd8362adb3b78309ffc3d6357cc436
Faaraan Kazi <faaraankazi@gmail.com>
2024-02-11 16:11:41 -0600

    Portfolio1

---
+++ build/precache-manifest.81df5ca9c4f3a1e7cb660701f0dad8d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bb77fbcb7f48ba8687a535b34477a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b88edaf988db4e0c1755",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "bc194cc927401ca1fc3d",
+    "url": "/static/js/2.e026a5a4.chunk.js"
+  },
+  {
+    "revision": "3b87527ae6d1ea6d2bb94094fb2d54e0",
+    "url": "/static/js/2.e026a5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88edaf988db4e0c1755",
+    "url": "/static/js/main.9c4754f0.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
